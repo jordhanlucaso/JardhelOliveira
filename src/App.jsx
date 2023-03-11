9af1b39306ff19b1ec1efe5962c909ff1b167104
Jordhan L. Oliveira <jos94@hotmail.com>
2023-03-11 09:31:35 +0100

    Website structure created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { BrowserRouter } from "react-router-dom";
+
 const App = () => {
 
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-patter bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
